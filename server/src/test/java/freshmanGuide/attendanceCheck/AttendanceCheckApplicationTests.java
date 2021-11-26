3044647be330a4c69e251b345adfd6e53a01bc49
seongbo_kim <seongbo_kim@tmax.co.kr>
2021-11-26 15:05:41 +0900

    first commit

---
+++ server/src/test/java/freshmanGuide/attendanceCheck/AttendanceCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package freshmanGuide.attendanceCheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendanceCheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
